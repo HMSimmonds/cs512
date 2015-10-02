aa0015fdbe6705924d6935b4a8070f4c2a940dfc
HMSimmonds <simmonds.hm@gmail.com>
2015-10-02 18:39:10 -0400

    Removed Resourcemanager template & updated main in server to launch the resource managers

--- src/server/ws/Main.java
+++ src/server/ws/Main.java
@@ -1,40 +1,20 @@
 package server.ws;
 
-import java.io.File;
-import org.apache.catalina.LifecycleException;
-import org.apache.catalina.startup.Tomcat;
+import server.ResourceManagerImpl;
 
 
 public class Main {
 
-    public static void main(String[] args) 
-    throws Exception {
-    
-        if (args.length != 3) {
+    public static void main(String[] args)
+            throws Exception {
+
+        if (args.length != 1) {
             System.out.println(
-                "Usage: java Main <service-name> <service-port> <deploy-dir>");
+                    "Usage: java Main <service-port>");
             System.exit(-1);
         }
-    
-        String serviceName = args[0];
-        int port = Integer.parseInt(args[1]);
-        String deployDir = args[2];
-    
-        Tomcat tomcat = new Tomcat();
-        tomcat.setPort(port);
-        tomcat.setBaseDir(deployDir);
 
-        tomcat.getHost().setAppBase(deployDir);
-        tomcat.getHost().setDeployOnStartup(true);
-        tomcat.getHost().setAutoDeploy(true);
-
-        //tomcat.addWebapp("", new File(deployDir).getAbsolutePath());
-
-        tomcat.addWebapp("/" + serviceName, 
-                new File(deployDir + "/" + serviceName).getAbsolutePath());
-
-        tomcat.start();
-        tomcat.getServer().await();
+        System.out.println("Creating resource manager with service port : " + Integer.parseInt(args[0]));
+        ResourceManagerImpl resourceManager = new ResourceManagerImpl(Integer.parseInt(args[0]));
     }
-    
 }
